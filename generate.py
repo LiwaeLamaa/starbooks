b65048260057b6441d614890dcb66a0849dd0a36
llamaa <liwaelamaa57@gmail.com>
2021-07-17 15:31:52 +0300

    enhance json generator

--- generate.py
+++ generate.py
@@ -8,7 +8,6 @@ import urllib
 files = []
 start_dir = os.getcwd()
 pattern   = "*.pdf"
-books_json = dict()
 books_list = []
 for dir,_,_ in os.walk(start_dir):
     files.extend(glob(os.path.join(dir,pattern)))
@@ -22,11 +21,14 @@ rel_files = []
 file_names = []
 for file in decoded_files:
     name = os.path.basename(file)
-    #rel_file = os.path.relpath(file, start_dir)
-    book_json_obj = {"name": name.decode("UTF-8").replace(".pdf",""), "link": file.decode("UTF-8").replace("\\","/")}
+    prefix = start_dir.replace("\\","/")
+    decoded_file = file.decode("UTF-8")
+    rel_file = os.path.relpath(decoded_file, prefix) 
+    rel_file = rel_file.replace("\\","/")
+    book_json_obj = {"name": name.decode("UTF-8").replace(".pdf",""), "link": rel_file }
     books_list.append(book_json_obj)
 
 #json_data = json.dumps(books_list)
-	
+books_json = {"books": books_list}
 with open('data1.json', 'w', encoding='utf-8') as outfile:
-    outfile.write(str(books_list))
+    outfile.write(str(books_json))
